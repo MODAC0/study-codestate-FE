5df1fc8f4e500a1cbf858db494bafabcaf1021ba
Hoyong Lee <gotoweb@eediom.com>
2019-01-02 00:19:42 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
-    "extends": "standard"
+    "extends": "standard",
+    rules: {
+        "no-undef-init": 0,
+        'semi': [2, "always"],
+        'space-before-function-paren': 'off'
+    }
 };
