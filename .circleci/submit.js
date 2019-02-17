3b897b7a197a3cb7b6f22f61344f5baa68234732
yun-sangho <contact@yunsangho.com>
2019-02-17 17:09:44 +0900

    change `submit.js` to send data to airtable

--- .circleci/submit.js
+++ .circleci/submit.js
@@ -1,48 +1,73 @@
-#!/usr/bin/env node
-
 const { exec } = require('child_process');
 const https = require('https');
+
+// Get sutdent data from student.json
+let studentInfo = require('../student.json')
+let {theClass, student, sprint} = studentInfo
+
 exec('npm test | grep -E \"[0-9]+\\s(passing|failing)\"', (err, stdout1, stderr) => {
-  if (err) {
-    return;
-  }
+    if (err) {
+        console.log(err)
+        throw new Error('can not take the test result')
+    }
 
-  exec('echo "$aws_lambda_apikey"', (err, apikey) => {
-    exec('echo "\n\n$CIRCLE_PR_USERNAME\n$CIRCLE_REPOSITORY_URL\n"', (err, stdout2) => {
-      console.log(`${stdout1}${stdout2}`);
+    // Get test result from the console and cleasing it for spread sheet
+    let matchWithPassing = stdout1.match(/([.\d,]+)[ ]+passing/)
+    let matchWithFailing = stdout1.match(/([.\d,]+)[ ]+failing/)
+    let passing = matchWithPassing ? Number(matchWithPassing[1]) : 0
+    let failing = matchWithFailing ? Number(matchWithFailing[1]) : 0
 
+    exec('echo "$airtable_api_key"', (err, apikey) => {
+      if (err) {
+        console.log(err);
+        throw new Error("echo command did not work right");
+      }
+    
+      if (apikey === "\n") {
+        throw new Error("There is not the airtable_api_key");
+      }  
+      
       const options = {
-        host: '3921zr9vkg.execute-api.ap-northeast-2.amazonaws.com',
-        path: '/default/getTestCaseResult',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': `${apikey}`.trim(),
-          'Access-Control-Request-Method': 'POST'
-        }
-      };
-      console.log(JSON.stringify(options.headers));
+            hostname: 'api.airtable.com',
+            path: '/v0/app8kEq9wXlsuffDy/Sprint',
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            Authorization: ' Bearer ' + apikey.trim()
+            }
+        };
+        console.log(JSON.stringify(options.headers));
 
-      const req = https.request(options, (res) => {
-        res.on('data', (chunk) => {
-          console.log('ok');
-          console.log(chunk.toString());
-          // callback(null, result);
+        const req = https.request(options, res => {
+          let data;
+          res.on("data", chunk => {
+            data += chunk;
+            // callback(null, result);
+          });
+          res.on("end", () => {
+            console.log(data);
+            if (data.includes("error")) {
+              throw new Error("There is an error on response from airtable.");
+            }
+          });
         });
-      });
+    
+        req.on('error', (e) => {
+            console.log(e)
+            throw new Error('data did not send to airtable correctly')
+            // callback(new Error('failure'));
+        });
 
-      req.on('error', (e) => {
-        console.log('error');
-        // callback(new Error('failure'));
-      });
-
-      // send the request
-      req.write(JSON.stringify({
-        'log': stdout1 + stdout2
-      }));
-      req.end();
-
-    });
-  });
+        // send the request
+        req.write(JSON.stringify({
+            'fields': {
+                'class': theClass,
+                'name':student,
+                'sprint': sprint,
+                'passed': passing,
+                'failed': failing,
+            }
+        }));
+        req.end();
+    })
 });
-
