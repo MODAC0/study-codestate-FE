f991c4c667c4c8205a89729f56b921db94f91e85
정진석 <jinsukjeong@jeongjinseog-ui-MacBook-Pro.local>
2019-02-25 20:39:24 +0900

    initial commit

---
+++ server/basic-server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+
+const PORT = 5000;
+
+const ip = 'localhost';
+
+const server = http.createServer((request, response) => {
+  let headers = defaultCorsHeader;
+  /* 
+  README.md 파일을 꼭 읽으시길 바랍니다. 모든 basic requirements가 정리되어 있습니다.  
+  HINT : 
+    request 의 method 와 url을 활용하여 클라이언트의 요청을 분기 할 수 있어야 합니다.
+    잘못된 요청은 잘못된 요청이라는 응답을 주어야 합니다.
+  */
+  console.log(
+    `http request method is ${request.method}, url is ${request.url}`
+  );
+  response.writeHead(200, headers);
+  response.end('hello mini-server sprints');
+});
+
+server.listen(PORT, ip, () => {
+  console.log(`http server listen on ${ip}:${PORT}`);
+});
+
+const defaultCorsHeader = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10
+};
