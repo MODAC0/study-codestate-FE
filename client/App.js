4ec00c25ae4355c79f3ee5d725575ce06ed6f1ce
정진석 <jinsukjeong@jeongjinseog-ui-MacBook-Pro.local>
2019-02-25 20:33:39 +0900

    draft

---
+++ client/App.js
@@ -0,0 +1,40 @@
+class App {
+  init() {
+    document
+      .querySelector('#to-upper-case')
+      .addEventListener('click', this.toUpperCase.bind(this));
+    document
+      .querySelector('#to-lower-case')
+      .addEventListener('click', this.toLowerCase.bind(this));
+  }
+  post(path, body) {
+    fetch(`http://localhost:5000/${path}`, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'content-type': 'text/json',
+        origin: 'localhost'
+      }
+    })
+      .then(res => res.json())
+      .then(res => {
+        this.render(res);
+      });
+  }
+  toLowerCase() {
+    const text = document.querySelector('.input-text').value;
+    this.post('lower', text);
+  }
+  toUpperCase() {
+    const text = document.querySelector('.input-text').value;
+    this.post('upper', text);
+  }
+  render(response) {
+    const resultWrapper = document.querySelector('#response-wrapper');
+    document.querySelector('.input-text').value = '';
+    resultWrapper.innerHTML = response;
+  }
+}
+
+const app = new App();
+app.init();
