824ecbb468be9f33bd93a42580ce98642a8544d2
정진석 <jin3378s@gmail.com>
2020-04-24 10:30:27 +0900

    Modify URCLASS URL path

--- .travis/submit.js
+++ .travis/submit.js
@@ -1,22 +1,22 @@
-const { exec } = require("child_process");
-const https = require("https");
+const { exec } = require('child_process');
+const https = require('https');
 const { URCLASS_URL, ASSESSMENT_ID, TRAVIS_PULL_REQUEST_SLUG } = process.env;
 
-if (TRAVIS_PULL_REQUEST_SLUG === "\n") {
-  throw new Error("github username is missing");
+if (TRAVIS_PULL_REQUEST_SLUG === '\n') {
+  throw new Error('github username is missing');
 }
 
-exec("mocha ./test/**/*.js --reporter json", (err, json, stderr) => {
+exec('mocha ./test/**/*.js --reporter json', (err, json, stderr) => {
   const result = JSON.parse(json);
-  const username = TRAVIS_PULL_REQUEST_SLUG.split("/")[0];
+  const username = TRAVIS_PULL_REQUEST_SLUG.split('/')[0];
 
   const options = {
     hostname: URCLASS_URL,
-    path: `/Prod/submit/`,
-    method: "POST",
+    path: `/production/submit/sprint`,
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json"
-    }
+      'Content-Type': 'application/json',
+    },
   };
 
   console.log(JSON.stringify(options));
@@ -25,33 +25,33 @@ exec("mocha ./test/**/*.js --reporter json", (err, json, stderr) => {
   const body = {
     assessment_id: ASSESSMENT_ID,
     githubUsername: username,
-    type: "mocha",
-    result: result
+    type: 'mocha',
+    result: result,
   };
 
   makeRequest(options, body);
 });
 
 function makeRequest(options, body) {
-  const req = https.request(options, res => {
+  const req = https.request(options, (res) => {
     let data;
-    res.on("data", chunk => {
+    res.on('data', (chunk) => {
       data += chunk;
     });
-    res.on("end", () => {
-      console.log("data from urclass is ", data);
+    res.on('end', () => {
+      console.log('data from urclass is ', data);
       if (res.statusCode >= 400) {
         if (res.statusCode === 400) {
-          throw new Error("invalid github username.");
+          throw new Error('invalid github username.');
         }
-        throw new Error("There is an error on response from urclass.");
+        throw new Error('There is an error on response from urclass.');
       }
     });
   });
 
-  req.on("error", e => {
+  req.on('error', (e) => {
     console.log(e);
-    throw new Error("data did not send to urclass");
+    throw new Error('data did not send to urclass');
   });
 
   req.write(JSON.stringify(body));
