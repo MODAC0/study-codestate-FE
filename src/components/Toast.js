cca83a04d0c6b7b9cbca6704e35632c70fb8ea9e
Hoyong Lee <igotoweb@gmail.com>
2021-01-08 02:10:06 +0900

    remove unnecessary code

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -4,14 +4,9 @@ export default function Toast({ text, dismissTime }) {
   const [isFading, setIsFading] = useState(false)
 
   useEffect(() => {
-    let mounted = true
     setTimeout(() => {
       setIsFading(true)
     }, dismissTime - 500)
-
-    return () => {
-      mounted = false
-    }
   })
 
   return (
