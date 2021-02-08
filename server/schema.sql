31ab7975994689389a711b2b1f5a525d3005117c
kimwunhae <kimeunhye1201@gmail.com>
2021-02-08 17:37:49 +0900

    add schema

---
+++ server/schema.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS `cmarket`;
+CREATE DATABASE `cmarket`;
+USE `cmarket`;
+
+CREATE TABLE `users` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+);
+
+CREATE TABLE `items` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `price` INT,
+  `image` BLOB,
+  `item_quantity` INT,
+);
+
+CREATE TABLE `orders` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `user_id` INT,
+  `total_price` INT,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE `order_items` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `order_id` INT,
+  `item_id` INT,
+  `order_quantity` INT,
+);
+
+ALTER TABLE `orders` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `order_items` ADD FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
+
+ALTER TABLE `order_items` ADD FOREIGN KEY (`item_id`) REFERENCES `items` (`id`);
+
+/*  Execute this file from the command line by typing:
+ *    mysql -u root < server/schema.sql
+ *  to create the database and the tables.*/
