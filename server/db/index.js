c4ff26d115da1dc4448af7f35389bf1a8aa7347d
hankyeolk <2247han@gmail.com>
2021-02-16 15:12:35 +0900

    add seed.sql and change some schema, db index.js

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,6 @@ const mysql = require("mysql");
 
 // process.env로 시작하는 모든 변수들은 환경 변수(environmental variables)입니다.
 // 환경 변수는 터미널에서 다음 명령을 이용하여 설정할 수 있습니다.
-// export DATABASE_SPRINT_PASSWORD=Gksruf94@
 const password = process.env.DATABASE_SPRINT_PASSWORD;
 const host = "localhost";
 const user = "root";
@@ -10,7 +9,7 @@ const user = "root";
 // 데이터베이스 연결을 만들고, 연결 객체를 export 하세요.
 // 연결에 필요한 몇가지 정보가 있습니다. 먼저 user는 root, 패스워드는 위 password 변수를 사용하세요.
 // 그리고 실제로 연결할 데이터베이스의 위치(host)는 host 변수를 사용하세요.
-// 데이터베이스 이름(database)은 "chat"로 지정하세요.
+// 데이터베이스 이름(database)은 "cmarket"로 지정하세요.
 
 const con = mysql.createConnection({
   host,
--- server/schema.sql
+++ server/schema.sql
@@ -2,7 +2,6 @@ DROP DATABASE IF EXISTS cmarket;
 CREATE DATABASE cmarket;
 USE cmarket;
 
-
 CREATE TABLE users (
   id INT AUTO_INCREMENT,
   username varchar(255),
@@ -38,8 +37,6 @@ ALTER TABLE orders ADD FOREIGN KEY (user_id) REFERENCES users (id);
 ALTER TABLE order_items ADD FOREIGN KEY (order_id) REFERENCES orders (id);
 
 ALTER TABLE order_items ADD FOREIGN KEY (item_id) REFERENCES items (id);
-
-INSERT INTO items (name, price, image) VALUES ('test', 23429, '../images/egg.png');
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
