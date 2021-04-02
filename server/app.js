c365dd48b798703f9c753523e32abd5dedb0c90c
피승권 <gwon@piseung-gwon-ui-MacBookPro.local>
2021-04-02 15:29:48 +0900

    modified server code

--- server/app.js
+++ server/app.js
@@ -26,19 +26,34 @@ app.use(session({
     resave: false
 }))
 
-app.get('/', (req, res) => {     
-    db.query('USE test',(err,results,fields) => {          
-        if(err){            
-            return res.status(503).send("데이터 베이스가 없습니다")           
-        }     
-        return res.status(200).send("데이터 베이스가 있습니다")
-    })    
+app.get('/', (req, res) => {
+    const { userId } = req.session
+    console.log(userId)
+    if(!userId){
+      res.status(200).send({
+          isLogin: false,
+          message: "아이디:김코딩, 비밀번호:1234를 입력해서 로그인해주세요"
+      })
+    }else {
+        db.query('USE test',(err) => {               
+            if(err){            
+                return res.status(503).send({
+                    isLogin: true,
+                    message: "데이터 베이스에 연결되지 않았습니다"
+                })           
+            }     
+            return res.status(200).send({
+                isLogin: true,
+                message: "데이터 베이스에 성공적으로 연결 되었습니다"
+            })
+        })
+    }  
 })
 
 app.post('/', (req, res) => {    
     const { username, password } = req.body
-    if(username !== 'KimCoding' && password !== '12341234'){
-        res.status(400).send('not authorized' );
+    if(username !== '김코딩' && password !== '1234'){
+        res.status(400).send('아이디나 비밀번호가 일치하지 않습니다');
     }else{                       
         req.session.userId = username;        
         res.send("OK");
--- server/db/connection.js
+++ server/db/connection.js
@@ -1,13 +1,19 @@
 const mysql = require('mysql');
 const dotenv = require('dotenv');
-const config = require('../config/config');
 dotenv.config();
 
-const con = mysql.createConnection(
-  config[process.env.NODE_ENV || 'development']
-);
+const con = mysql.createConnection({
+  host : process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,  
+  port: process.env.DATABASE_PORT
+});
 
-con.connect()    
+con.connect((err) => {    
+  if(err){
+    con.end() 
+  }
+})    
 
 module.exports = con;
 
