b210fc7c8380d94d231cb772991e6bd76d5a3cbd
TaylorKim <skdmlakxn45@naver.com>
2021-04-06 17:21:55 +0900

    Update
    Update

--- statesairline/controller/bookcontroller.js
+++ statesairline/controller/bookcontroller.js
@@ -4,46 +4,42 @@ const { v4: uuidv4 } = require('uuid');
 module.exports = {
     userlookup : (req, res)=>{
         try{
-            if(req.query){
-                if(req.query.flightid){
-                    let list = new Array();
-                    reservationlist.map((item)=>{
-                        if(item.flight_guid === req.query.flightid){
-                            list.push(item);
-                        }
-                    })
-                    console.log(`[GET] Success : /book?flight=${req.query.flightid}`);
-                    res.json(list);
-                }
-                if(req.query.phone){
-                    let data;
-                    reservationlist.map((userdata)=>{
-                        if(userdata.phone === req.query.phone){
-                            flightlist.map((flightdata)=>{
-                                if(flightdata.uuid === userdata.flight_guid){
-                                    data = {
-                                        departure : flightdata.departure,
-                                        destination : flightdata.destination,
-                                        departure_times : flightdata.departure_times,
-                                        arrival_times : flightdata.arrival_times,
-                                        name : userdata.name,
-                                        phone : userdata.phone
-                                    }
-                                }
-                            });
-                        }
-                    });
-                    console.log(`[GET] Success : /book?phone=${req.query.phone}`);
-                    res.json(data);
-                }
-                res.status(404).send('Error : Not Found data')
-            }else{
-                console.log(`[GET] Success : /book`);
-                res.json(reservationlist);
+            if(req.query.flightid !== undefined){
+                let list = new Array();
+                reservationlist.map((item)=>{
+                    if(item.flight_guid === req.query.flightid){
+                        list.push(item);
+                    }
+                })
+                console.log(`[GET] Success : /book?flight=${req.query.flightid}`);
+                return res.status(200).json(list);
             }
+            if(req.query.phone !== undefined){
+                let data;
+                reservationlist.map((userdata)=>{
+                    if(userdata.phone === req.query.phone){
+                        flightlist.map((flightdata)=>{
+                            if(flightdata.uuid === userdata.flight_guid){
+                                data = {
+                                    departure : flightdata.departure,
+                                    destination : flightdata.destination,
+                                    departure_times : flightdata.departure_times,
+                                    arrival_times : flightdata.arrival_times,
+                                    name : userdata.name,
+                                    phone : userdata.phone
+                                }
+                            }
+                        });
+                    }
+                });
+                console.log(`[GET] Success : /book?phone=${req.query.phone}`);
+                return res.status(200).json(data);
+            }
+            console.log(`[GET] Success : /book`);
+            return res.status(200).json(reservationlist);
         }catch(error){
             console.error(`[GET] Error : /book ${error}`);
-            res.send(`[GET] Failed : Not found reservationdata`);
+            return res.status(506).send(`[GET] Failed : Not found reservationdata`);
         }
     },
 
@@ -52,27 +48,23 @@ module.exports = {
             let data = req.body;
             reservationlist.push(new bookdata(uuidv4(), data.flight_guid, data.name, data.phone));
             console.log(`[POST] Success : /book`);
-            res.send(`[POST] Success : Create reservationdata`);
+            return res.status(200).send(`[POST] Success : Create reservationdata`);
         } catch (error) {
             console.error(`[POST] Error : /book ${error}`);
-            res.send(`[POST] Failed : Not Create reservationdata`);
+            return res.status(506).send(`[POST] Failed : Not Create reservationdata`);
         }
     },
 
     userdelete : (req, res) => {
         try {
-            let list = new Array();
-            reservationlist.map((item)=>{
-                if (item.uuid !== req.params.id) {
-                    list.push(reservationlist[i]);
-                }
+            reservationlist.map((item, index)=>{
+                reservationlist.splice(index, 1)
             });
-            reservationlist = list;
             console.log(`[delete] Success : /delete/reservationdata/:idx`);
-            res.send(`[delete] Success : delete flightdata [${req.params.id}]`);
+            return res.status(200).send(`[delete] Success : delete flightdata [${req.params.id}]`);
         } catch (error) {
             console.error(`[delete] Error /delete/reserviontdata/:idx : ${error}`);
-            res.send(`[delete] Failed : Not delete reservationdata`);
+            return res.status(506).send(`[delete] Failed : Not delete reservationdata`);
         }
     }
 
--- statesairline/controller/flightcontroller.js
+++ statesairline/controller/flightcontroller.js
@@ -3,41 +3,39 @@ const {flightlist} = require('../Repositories/statesRepositories');
 module.exports = {
     flightlookup : (req, res)=>{
         try{
-            if(req.query){
-                // 조건에 따른 항공편 조회
-                // 시간에 따른 필터링
-                if(req.query.departure_times && req.query.arrival_times){
-                    let list = new Array();
-                    flightlist.map((item)=>{
-                        if(flightlist.departure_times === req.query.departure_times 
-                            && flightlist.arrival_times === req.query.arrival_times){
-                                list.push(item);
-                        }
-                    });
-                    console.log(`[GET] Success : /flight?departure_times=${req.query.departure_times}&arrival_times=${req.query.arrival_times}`);
-                    res.json(list);
-                }
-                // 공항에 따른 필터링
-                if(req.query.departure && req.query.destination){
-                    let list = new Array();
-                    flightlist.map((item)=>{
-                        if(flightlist.departure === req.query.departure 
-                            && flightlist.destination === req){
-                                list.push(item);
-                        }
-                    });
-                    console.log(`[GET] Success : /flight?departure=${req.query.departure}&destination=${req.query.destination}`);
-                    res.json(list);
-                }
-                res.status(404).send('Error : Not Found data')
-            }else{
-                // 쿼리가 없으면 전체 데이터 전달
-                console.log(`[GET] Success : /flight`);
-                res.json(flightlist);
+            // 조건에 따른 항공편 조회
+            // 시간에 따른 필터링
+            if(req.query.departure_times !== undefined && req.query.arrival_times !== undefined){
+                let list = new Array();
+                flightlist.map((item)=>{
+                    if(item.departure_times === req.query.departure_times 
+                        && item.arrival_times === req.query.arrival_times){
+                            list.push(item);
+                    }
+                });
+                console.log(`[GET] Success : /flight?departure_times=${req.query.departure_times}&arrival_times=${req.query.arrival_times}`);
+                return res.status(200).json(list);
             }
+            // 공항에 따른 필터링
+            if(req.query.departure !== undefined && req.query.destination !== undefined){
+                let list = new Array();
+                flightlist.map((item)=>{
+                    if(item.departure === req.query.departure 
+                        && item.destination === req.query.destination){
+                            list.push(item);
+                    }
+                });
+                console.log(`[GET] Success : /flight?departure=${req.query.departure}&destination=${req.query.destination}`);
+                return res.status(200).json(list);
+            }   
+
+            // 쿼리가 없으면 전체 데이터 전달
+            console.log(`[GET] Success : /flight`);
+            res.json(flightlist);
+
         }catch(error){
             console.error(`[GET] Error : /flight ${error}`);
-            res.status(506).send(`[GET] Failed : Not found flight`);
+            return res.status(506).send(`[GET] Failed : Not found flight`);
         }
     },
 
@@ -51,18 +49,18 @@ module.exports = {
                     }
                 });
                 console.log(`[GET] Success : /flight/:id`);
-                res.send(data);
+                return res.status(200).send(data);
             }
         }catch(error){
             console.error(`[GET] Error : /flight/:id ${error}`);
-            res.status(506).send(`[GET] Failed : Not found flight`);
+            return res.status(506).send(`[GET] Failed : Not found flight`);
         }
     },
 
     flightupdate : (req, res)=>{
         try{
             flightlist.map((item)=>{
-                if(req.params.id == item.uuid){
+                if(req.params.id === item.uuid){
                     if(!req.params.departure){
                         item.departure = req.body.departure; 
                     }
@@ -78,10 +76,10 @@ module.exports = {
                 }
             });
             console.log(`[PUT] Success : /flight/:id`);
-            res.send(`[PUT] Success : Update reservationdata`);
+            return res.status(200).send(`[PUT] Success : Update reservationdata`);
         }catch(error){
             console.error(`[PUT] Error : /flight/:id ${error}`);
-            res.status(506).send(`[PUT] Failed : Not Update flight ${req.params.id}`);
+            return res.status(506).send(`[PUT] Failed : Not Update flight ${req.params.id}`);
         }
     }
 }
