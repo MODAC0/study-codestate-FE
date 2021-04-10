d2eb867ad332678e16b8c6ad4966606599314dc3
피승권 <gwon@piseung-gwon-ui-MacBookPro.local>
2021-04-10 13:16:40 +0900

    main.js messaged modified

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -15,7 +15,7 @@ class Main extends Component {
   render () {
     return (
             <div className="main-container">
-                <div className="session">세션을 정상적으로 받았습니다</div>
+                <div className="session">토큰을 정상적으로 받았습니다</div>
                 <div className="db">데이터 베이스 연결 상태를 확인하세요</div>
                 <button type="submit" onClick={this.handleLogout}>로그아웃</button>
             </div>
