cfc42d829e16723aff92c783060a9b35948b3c18
TaylorKim <skdmlakxn45@naver.com>
2021-04-20 18:37:05 +0900

    fix : json 데이터 시간 형식 변경

--- statesairline/repository/flightlist.js
+++ statesairline/repository/flightlist.js
@@ -3,36 +3,36 @@ module.exports = [
     uuid: 'af6fa55c-da65-47dd-af23-578fdba40bed',
     departure: 'ICN',
     destination: 'CJU',
-    departure_times: '2021-12-02 12:00:00',
-    arrival_times: '2021-12-03 12:00:00'
+    departure_times: '2021-12-02T12:00:00',
+    arrival_times: '2021-12-03T12:00:00'
   },
   {
     uuid: 'af6fa55c-da65-47dd-af23-578fdba40byd',
     departure: 'ICN',
     destination: 'CJU',
-    departure_times: '2021-12-03 12:00:00',
-    arrival_times: '2021-12-03 12:00:00'
+    departure_times: '2021-12-03T12:00:00',
+    arrival_times: '2021-12-03T12:00:00'
   },
   {
     uuid: 'af6fa55c-da65-47dd-af23-578fdba48bed',
     departure: 'ICN',
     destination: 'CJU',
-    departure_times: '2021-12-03 12:00:00',
-    arrival_times: '2021-12-04 12:00:00'
+    departure_times: '2021-12-03T12:00:00',
+    arrival_times: '2021-12-04T12:00:00'
   },
   {
     uuid: 'af6fa55c-da65-47dd-af23-578fdbr40bed',
     departure: 'ICN',
     destination: 'CJU',
-    departure_times: '2021-12-03 12:00:00',
-    arrival_times: '2021-12-04 12:00:00'
+    departure_times: '2021-12-03T12:00:00',
+    arrival_times: '2021-12-04T12:00:00'
   },
   {
     uuid: 'af6fa55c-da65-47dd-af23-578fd7a40bed',
     departure: 'ICN',
     destination: 'CJU',
-    departure_times: '2021-12-02 12:00:00',
-    arrival_times: '2021-12-03 12:00:00'
+    departure_times: '2021-12-02T12:00:00',
+    arrival_times: '2021-12-03T12:00:00'
   },
   {
     uuid: 'af6fa55c-da65-47dd-af23-578fdba40bod',
@@ -45,35 +45,35 @@ module.exports = [
     uuid: 'af6fa55c-da65-47dd-af23-578fdba44bed',
     departure: 'ICN',
     destination: 'CJU',
-    departure_times: '2021-12-03 12:00:00',
-    arrival_times: '2021-12-03 12:00:00'
+    departure_times: '2021-12-03T12:00:00',
+    arrival_times: '2021-12-03T12:00:00'
   },
   {
     uuid: 'af6fa55c-da65-47dd-af23-578fdba42bed',
     departure: 'CJU',
     destination: 'ICN',
-    departure_times: '2021-12-03 12:00:00',
-    arrival_times: '2021-12-04 12:00:00'
+    departure_times: '2021-12-03T12:00:00',
+    arrival_times: '2021-12-04T12:00:00'
   },
   {
     uuid: 'af6fa55c-da65-47dd-af23-578fdba41bed',
     departure: 'CJU',
     destination: 'ICN',
-    departure_times: '2021-12-03 12:00:00',
-    arrival_times: '2021-12-03 12:00:00'
+    departure_times: '2021-12-03T12:00:00',
+    arrival_times: '2021-12-03T12:00:00'
   },
   {
     uuid: 'af6fa55c-da65-47dd-af23-578fdba99bed',
     departure: 'CJU',
     destination: 'ICN',
-    departure_times: '2021-12-03 12:00:00',
-    arrival_times: '2021-12-04 12:00:00'
+    departure_times: '2021-12-03T12:00:00',
+    arrival_times: '2021-12-04T12:00:00'
   },
   {
     uuid: 'af6fa55c-da65-47dd-af23-578fdba50bed',
     departure: 'CJU',
     destination: 'ICN',
-    departure_times: '2021-12-02 12:00:00',
-    arrival_times: '2021-12-03 12:00:00'
+    departure_times: '2021-12-02T12:00:00',
+    arrival_times: '2021-12-03T12:00:00'
   }
 ];
