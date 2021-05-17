9241566c4bb1c1e5dcd75adb6661f5c062ad6079
TaylorKim <skdmlakxn45@naver.com>
2021-05-17 18:09:46 +0900

    update : 펀치홀 추가

--- statesairline/controller/bookController.js
+++ statesairline/controller/bookController.js
@@ -6,37 +6,27 @@ module.exports = {
   // [GET] /book 요청을 수행합니다.
   // 전체 데이터 혹은 요청 된 flight_id, phone 값과 동일한 예약 데이터를 조회합니다.
   findById: async (req, res) => {
-    if (req.query.flight_id !== undefined) {
-      const filtered = booking.filter(item => item.flight_uuid === req.query.flight_id);
-      return res.status(200).json(filtered);
-    }
-    if (req.query.phone !== undefined) {
-      const filtered = booking.filter(item => item.phone === req.query.phone);
-      if (filtered.length === 0) {
-        return res.status(404).json([]);
-      }
-      const [{ uuid, flight_uuid, name, phone }] = filtered;
-      return res.status(200).json({ uuid, flight_uuid, name, phone });
-    }
+    //DOTO : 
+
+
     return res.status(200).json(booking);
   },
 
   // [POST] /book 요청을 수행합니다.
   // 요청 된 예약 데이터를 저장합니다.
   create: async (req, res) => {
-    const { flight_uuid, name, phone } = req.body;
-    booking.push({
-      flight_uuid,
-      name,
-      phone
-    });
+    //DOTO : 
+
+
     return res.status(201).json({ message: 'Create success!' });
   },
 
   // [DELETE] /book?phone={phone} 요청을 수행합니다.
   // 요청 된 phone 값과 동일한 예약 데이터를 삭제합니다.
   deleteById: async (req, res) => {
-    booking = booking.filter(item => req.query.phone !== item.phone);
+    //DOTO : 
+
+    
     return res.status(200).json(booking);
   }
 };
