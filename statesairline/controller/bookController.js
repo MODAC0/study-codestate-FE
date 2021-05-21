d4ab6fbb3c8658b2bb36c6e966b59f689257710e
TaylorKim <skdmlakxn45@naver.com>
2021-05-21 15:27:44 +0900

    fix : 주석 수정

--- statesairline/controller/bookController.js
+++ statesairline/controller/bookController.js
@@ -6,7 +6,7 @@ module.exports = {
   // [GET] /book 요청을 수행합니다.
   // 전체 데이터 혹은 요청 된 flight_id, phone 값과 동일한 예약 데이터를 조회합니다.
   findById: async (req, res) => {
-    //DOTO : 
+    //DOTO
 
 
     return res.status(200).json(booking);
@@ -15,7 +15,7 @@ module.exports = {
   // [POST] /book 요청을 수행합니다.
   // 요청 된 예약 데이터를 저장합니다.
   create: async (req, res) => {
-    //DOTO : 
+    //DOTO
 
 
     return res.status(201).json({ message: 'Create success!' });
@@ -24,7 +24,7 @@ module.exports = {
   // [DELETE] /book?phone={phone} 요청을 수행합니다.
   // 요청 된 phone 값과 동일한 예약 데이터를 삭제합니다.
   deleteById: async (req, res) => {
-    //DOTO : 
+    //DOTO
 
     
     return res.status(200).json(booking);
--- statesairline/controller/flightController.js
+++ statesairline/controller/flightController.js
@@ -4,14 +4,15 @@ module.exports = {
   // [GET] /flight
   // 요청 된 departure_times, arrival_times, destination, departure 값과 동일한 값을 가진 항공편 데이터를 조회합니다.
   findAll: async (req, res) => {
-    //DOTO : 
+    //DOTO 
+    
 
     return res.json(flights);
   },
   // [GET] /fligjt/{:id}
   // 요청 된 id 값과 동일한 uuid 값을 가진 항공편 데이터를 조회합니다.
   findById: async (req, res) => {
-    //DOTO : 
+    //DOTO 
 
 
   },
@@ -20,7 +21,7 @@ module.exports = {
   // 요청 된 id 값과 동일한 uuid 값을 가진 항공편 데이터를 요쳥 된 Body 데이터로 수정합니다.
   update: async (req, res) => {
     let data;
-    //DOTO : 
+    //DOTO 
 
 
     
