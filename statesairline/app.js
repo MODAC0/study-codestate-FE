0e82b8047042442622a9b591ac1059a91b3a43de
TaylorKim <skdmlakxn45@naver.com>
2021-06-01 13:11:23 +0900

    fix

--- statesairline/app.js
+++ statesairline/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 81;
+const port = 3001;
 
 const flightRouter = require('./router/flightRouter');
 const bookRouter = require('./router/bookRouter');
