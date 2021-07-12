41b513436136cb5335022737c5f935ffc3a94239
kimwunhae <kimeunhye1201@gmail.com>
2021-07-12 15:12:19 +0900

    rm package and revise testcase

--- src/reducers/__test__/index.test.js
+++ src/reducers/__test__/index.test.js
@@ -1,92 +1,91 @@
-import { ADD_TO_CART, REMOVE_FROM_CART, SET_QUANTITY } from "../../actions/index";
+import {
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  SET_QUANTITY,
+} from "../../actions/index";
 import { initialState } from "../initialState";
 import itemReducer from "../itemReducer";
 
 describe("Item Reducer", () => {
   it("ADD_TO_CART 액션에 따라 cartItems 상태가 변해야 합니다", () => {
     expect(
-      itemReducer(
-        initialState,
-        {
-          type: ADD_TO_CART,
-          payload: {
-            itemId: 4,
-            quantity: 1
-          },
-        }
-      )
+      itemReducer(initialState, {
+        type: ADD_TO_CART,
+        payload: {
+          itemId: 4,
+          quantity: 1,
+        },
+      })
     ).toEqual({
       items: initialState.items,
       cartItems: [...initialState.cartItems, { itemId: 4, quantity: 1 }],
-      notifications: []
+      notifications: [],
     });
   });
   it("REMOVE_FROM_CART 액션에 따라 cartItems 상태가 변해야 합니다", () => {
     expect(
-      itemReducer(
-        initialState,
-        {
-          type: REMOVE_FROM_CART,
-          payload: {
-            itemId: 1
-          },
-        }
-      )
+      itemReducer(initialState, {
+        type: REMOVE_FROM_CART,
+        payload: {
+          itemId: 1,
+        },
+      })
     ).toEqual({
       items: initialState.items,
       cartItems: [
         {
-          "itemId": 5,
-          "quantity": 7
+          itemId: 5,
+          quantity: 7,
         },
         {
-          "itemId": 2,
-          "quantity": 3
-        }],
-      notifications: []
+          itemId: 2,
+          quantity: 3,
+        },
+      ],
+      notifications: [],
     });
   });
   it("SET_QUANTITY 액션에 따라 cartItems 상태가 변해야 합니다", () => {
     expect(
-      itemReducer(
-        initialState,
-        {
-          type: SET_QUANTITY,
-          payload: {
-            itemId: 1,
-            quantity: 8
-          },
-        }
-      )
+      itemReducer(initialState, {
+        type: SET_QUANTITY,
+        payload: {
+          itemId: 1,
+          quantity: 8,
+        },
+      })
     ).toEqual({
       items: initialState.items,
-      cartItems: [{
-        "itemId": 1,
-        "quantity": 8
-      },
-      {
-        "itemId": 5,
-        "quantity": 7
-      },
-      {
-        "itemId": 2,
-        "quantity": 3
-      }],
-      notifications: []
+      cartItems: [
+        {
+          itemId: 1,
+          quantity: 8,
+        },
+        {
+          itemId: 5,
+          quantity: 7,
+        },
+        {
+          itemId: 2,
+          quantity: 3,
+        },
+      ],
+      notifications: [],
     });
   });
   it("리듀서는 다른 상태의 값을 보존해야 합니다", () => {
     expect(
       itemReducer(
         {
-          mustkeep: "other states", cartItems: [],
+          mustkeep: "other states",
+          cartItems: [],
         },
         {
           type: ADD_TO_CART,
           payload: {
             itemId: 6,
-            quantity: 1
-          }
+            quantity: 1,
+          },
         }
       )
     ).toEqual({
@@ -95,32 +94,34 @@ describe("Item Reducer", () => {
     });
     expect(
       itemReducer(
-        { mustkeep: "other states", cartItems: [] },
         {
-          type: SET_QUANTITY,
-          payload: {
-            itemId: 6,
-            quantity: 3
-          }
-        }
-      )
-    ).toEqual({
-      mustkeep: "other states",
-      cartItems: [{ itemId: 6, quantity: 3 }],
-    });
-    expect(
-      itemReducer(
-        { mustkeep: "other states", cartItems: [] },
+          mustkeep: "other states",
+          cartItems: [
+            {
+              itemId: 6,
+              quantity: 1,
+            },
+            {
+              itemId: 1,
+              quantity: 1,
+            },
+          ],
+        },
         {
           type: REMOVE_FROM_CART,
           payload: {
             itemId: 1,
-          }
+          },
         }
       )
     ).toEqual({
       mustkeep: "other states",
-      cartItems: [],
+      cartItems: [
+        {
+          itemId: 6,
+          quantity: 1,
+        },
+      ],
     });
   });
 });
