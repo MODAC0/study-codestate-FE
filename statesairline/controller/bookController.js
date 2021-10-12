77015f285f5ad69a3b9b4654003980fcc29c4d0f
oyeon-kwon <rnjsdhdus93@gmail.com>
2021-10-12 19:26:27 +0900

    fix: POST요청 응답을 RESTful하게 수정

--- statesairline/controller/bookController.js
+++ statesairline/controller/bookController.js
@@ -14,11 +14,14 @@ module.exports = {
 
   // [POST] /book 요청을 수행합니다.
   // 요청 된 예약 데이터를 저장합니다.
+  // 응답으로는 book_id를 리턴합니다.
+  // Location Header로 예약 아이디를 함께 보내준다면 RESTful한 응답에 더욱 적합합니다.
+    // 참고 링크: https://www.vinaysahni.com/best-practices-for-a-pragmatic-restful-api#useful-post-responses
   create: async (req, res) => {
     //TODO:
 
 
-    return res.status(201).json({ message: 'Create success!' });
+    return res.status(201).json({});
   },
 
   // [DELETE] /book?phone={phone} 요청을 수행합니다.
