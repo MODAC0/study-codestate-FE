0d3618b080079d428cb18b9d19377a74962838f3
oyeon-kwon <rnjsdhdus93@gmail.com>
2021-10-14 00:07:13 +0900

    fix: async 삭제

--- statesairline/controller/airportController.js
+++ statesairline/controller/airportController.js
@@ -3,7 +3,7 @@ const airports = require('../repository/airportList');
 module.exports = {
   // [GET] /airport?query={query} 요청을 수행합니다.
   // 공항 이름 자동완성 기능을 수행합니다!
-  findAll: async (req, res) => {
+  findAll: (req, res) => {
     if (req.query.query !== undefined) {
       console.log(req.query.query);
       const list = airports.filter((item) => {
--- statesairline/controller/bookController.js
+++ statesairline/controller/bookController.js
@@ -5,7 +5,7 @@ let booking = [];
 module.exports = {
   // [GET] /book 요청을 수행합니다.
   // 전체 데이터 혹은 요청 된 flight_uuid, phone 값과 동일한 예약 데이터를 조회합니다.
-  findById: async (req, res) => {
+  findById: (req, res) => {
     //TODO:
 
 
@@ -17,7 +17,7 @@ module.exports = {
   // 응답으로는 book_id를 리턴합니다.
   // Location Header로 예약 아이디를 함께 보내준다면 RESTful한 응답에 더욱 적합합니다.
     // 참고 링크: https://www.vinaysahni.com/best-practices-for-a-pragmatic-restful-api#useful-post-responses
-  create: async (req, res) => {
+  create: (req, res) => {
     //TODO:
 
 
@@ -26,7 +26,7 @@ module.exports = {
 
   // [DELETE] /book?phone={phone} 요청을 수행합니다.
   // 요청 된 phone 값과 동일한 예약 데이터를 삭제합니다.
-  deleteById: async (req, res) => {
+  deleteById: (req, res) => {
     //TODO:
 
     
