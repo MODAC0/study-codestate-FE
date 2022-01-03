0a34458c772d3082ba388cd072e45a366da9060a
Hoyong Lee <igotoweb@gmail.com>
2022-01-03 20:11:05 +0900

    feat: 서버에 mysql2/promise 적용, db 주소 고정, Dockerfile 추가

--- server/app.js
+++ server/app.js
@@ -2,10 +2,94 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { authToken } = require('./middleware/token');
-const db = require('./db/connection');
-
+const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
+
+
+// code from https://github.com/GoogleCloudPlatform/nodejs-docs-samples/blob/HEAD/cloud-sql/mysql/mysql/server.js
+// [START cloud_sql_mysql_mysql_create_tcp]
+const createTcpPool = async config => {
+  // Extract host and port from socket address
+
+  // Establish a connection to the database
+  return mysql.createPool({
+    user: process.env.DATABASE_USER, // e.g. 'my-db-user'
+    password: process.env.DATABASE_PASSWORD, // e.g. 'my-db-password'
+    // database: 'process.env.DB_NAME', // e.g. 'my-database'
+    host: process.env.DATABASE_HOST, // e.g. '127.0.0.1'
+    port: process.env.DATABASE_PORT, // e.g. '3306'
+    // ... Specify additional properties here.
+    ...config
+  });
+};
+
+const createPool = async () => {
+  const config = {
+    // [START cloud_sql_mysql_mysql_limit]
+    // 'connectionLimit' is the maximum number of connections the pool is allowed
+    // to keep at once.
+    connectionLimit: 5,
+    // [END cloud_sql_mysql_mysql_limit]
+
+    // [START cloud_sql_mysql_mysql_timeout]
+    // 'connectTimeout' is the maximum number of milliseconds before a timeout
+    // occurs during the initial connection to the database.
+    connectTimeout: 10000, // 10 seconds
+    // 'acquireTimeout' is the maximum number of milliseconds to wait when
+    // checking out a connection from the pool before a timeout error occurs.
+    acquireTimeout: 10000, // 10 seconds
+    // 'waitForConnections' determines the pool's action when no connections are
+    // free. If true, the request will queued and a connection will be presented
+    // when ready. If false, the pool will call back with an error.
+    waitForConnections: true, // Default: true
+    // 'queueLimit' is the maximum number of requests for connections the pool
+    // will queue at once before returning an error. If 0, there is no limit.
+    queueLimit: 0 // Default: 0
+    // [END cloud_sql_mysql_mysql_timeout]
+
+    // [START cloud_sql_mysql_mysql_backoff]
+    // The mysql module automatically uses exponential delays between failed
+    // connection attempts.
+    // [END cloud_sql_mysql_mysql_backoff]
+  };
+
+  return createTcpPool(config);
+};
+
+const ensureSchema = async pool => {
+  // Wait for tables to be created (if they don't already exist).
+  await pool.query('select 1+1');
+  console.log('Ensured SQL works');
+};
+
+const createPoolAndEnsureSchema = async () =>
+  await createPool()
+    .then(async pool => {
+      await ensureSchema(pool);
+      return pool;
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+
+let pool;
 app.use(express.json());
+app.use(async (req, res, next) => {
+  if (pool) {
+    return next();
+  }
+  try {
+    pool = await createPoolAndEnsureSchema();
+    next();
+  } catch (err) {
+    console.log(err);
+    return next(err);
+  }
+});
+
 const port = 80;
 
 app.use(
@@ -31,18 +115,20 @@ app.get('/', (req, res) => {
 
 app.get('/status', authToken, (req, res) => {
   if (req.username) { // jwt 토큰이 존재할 경우 데이터베이스 연결 여부 조회
-    db.query('use test', (err) => {
-      if (err) {
-        return res.status(200).send({
+    pool.query('select 1+1')
+      .then(() => {
+        res.status(200).send({
+          isLogin: true,
+          isConnectedToDatabase: true
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(200).send({
           isLogin: true,
           isConnectedToDatabase: false
         });
-      }
-      return res.status(200).send({
-        isLogin: true,
-        isConnectedToDatabase: true
       });
-    });
   }
 });
 
