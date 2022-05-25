5db49c42d3591ffa6f6fda82b878a9c6950ca796
Jekyung-Min <cwm7485@naver.com>
2022-05-25 15:58:44 +0900

    fix: react-router-dom 6버전에 맞게 테스트 케이스 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// TODO : React Router DOM을 설치 후, import 구문을 이용하여 BrowserRouter, Route, Switch 컴포넌트를 불러옵니다.
+// TODO : React Router DOM을 설치 후, import 구문을 이용하여 BrowserRouter, Routes, Route 컴포넌트를 불러옵니다.
 
 import Sidebar from './Sidebar';
 import Tweets from './Pages/Tweets';
@@ -14,11 +14,9 @@ const App = (props) => {
         <main>
           <Sidebar />
           <section className="features">
-            {/* <Route exact path="/"> */}
-              <Tweets />
-            {/* </Route> */}
             {/* TODO : 유어클래스를 참고해서, 테스트 케이스를 통과하세요.
-            TODO : React Router DOM 설치 후 BrowserRouter, Route의 주석을 해제하고 Swtich 컴포넌트를 적절하게 작성합니다. */}
+            TODO : React Router DOM 설치 후 BrowserRouter, Routes, Route의 주석을 해제하고 Routes, Route 컴포넌트를 적절하게 작성합니다. */}
+            {/* Route 예시: <Route path="/" element={<Tweets />}></Route> */}
           </section>
         </main>
       </div>
