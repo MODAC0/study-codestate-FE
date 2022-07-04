d4dcfa94c220d5855331f1726093984768dff989
kimdaeyoon <eodbszla@gmail.com>
2022-07-04 11:18:44 +0900

    Edit react-router-dom in App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
-import React, { useState } from 'react';
-import Nav from './components/Nav';
-import ItemListContainer from './pages/ItemListContainer';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import ShoppingCart from './pages/ShoppingCart';
-import { initialState } from './assets/state';
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import ItemListContainer from "./pages/ItemListContainer";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ShoppingCart from "./pages/ShoppingCart";
+import { initialState } from "./assets/state";
 
 function App() {
-
   const [items, setItems] = useState(initialState.items);
   const [cartItems, setCartItems] = useState(initialState.cartItems);
 
   return (
     <Router>
       <Nav />
-      <Switch>
-        <Route exact={true} path="/">
-          <ItemListContainer items={items} />
-        </Route>
-        <Route path="/shoppingcart">
-          <ShoppingCart cartItems={cartItems} items={items} />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/" element={<ItemListContainer items={items} />} />
+        <Route
+          path="/shoppingcart"
+          element={<ShoppingCart cartItems={cartItems} items={items} />}
+        />
+      </Routes>
     </Router>
   );
 }
