6ee76dacbf80344eff01e1403220eddb6178b648
Citysquirrel <86960007+Citysquirrel@users.noreply.github.com>
2022-07-31 20:04:00 +0900

    feat: apply global style

--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ import Tweets from './Pages/Tweets';
 // TODO : MyPage, About 컴포넌트를 import 합니다.
 
 import './App.css';
+import './global-style.css';
 
 const App = (props) => {
   return (
     // <BrowserRouter>
-      <div className="App">
-        <main>
-          <Sidebar />
-          <section className="features">
-            {/* TODO : 유어클래스를 참고해서, 테스트 케이스를 통과하세요.
+    <div className="App">
+      <main>
+        <Sidebar />
+        <section className="features">
+          {/* TODO : 유어클래스를 참고해서, 테스트 케이스를 통과하세요.
             TODO : React Router DOM 설치 후 BrowserRouter, Routes, Route의 주석을 해제하고 Routes, Route 컴포넌트를 적절하게 작성합니다. */}
-            {/* Route 예시: <Route path="/" element={<Tweets />}></Route> */}
-          </section>
-        </main>
-      </div>
+          {/* Route 예시: <Route path="/" element={<Tweets />}></Route> */}
+        </section>
+      </main>
+    </div>
     // </BrowserRouter>
   );
 };
