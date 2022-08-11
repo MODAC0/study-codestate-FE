410f8b7ccb6e8b51f5b93eb1034cccdf63da670d
Jekyung-Min <cwm7485@naver.com>
2022-08-11 17:15:29 +0900

    feat: modify controller

--- statesairline/controller/bookController.js
+++ statesairline/controller/bookController.js
@@ -4,29 +4,24 @@ let booking = [];
 
 module.exports = {
   // [GET] /book 요청을 수행합니다.
-  // 전체 데이터 혹은 요청 된 flight_uuid, phone 값과 동일한 예약 데이터를 조회합니다.
+  // 전체 예약 데이터를 조회합니다.
   findById: (req, res) => {
-    // TODO:
-
     return res.status(200).json(booking);
   },
 
   // [POST] /book 요청을 수행합니다.
   // 요청 된 예약 데이터를 저장합니다.
-  // 응답으로는 book_id를 리턴합니다.
-  // Location Header로 예약 아이디를 함께 보내준다면 RESTful한 응답에 더욱 적합합니다.
-  // 참고 링크: https://www.vinaysahni.com/best-practices-for-a-pragmatic-restful-api#useful-post-responses
   create: (req, res) => {
     // TODO:
 
     return res.status(201).json({});
   },
 
-  // [DELETE] /book?phone={phone} 요청을 수행합니다.
-  // 요청 된 phone 값과 동일한 예약 데이터를 삭제합니다.
+  // Optional
+  // [DELETE] /book/:id/:phone 요청을 수행합니다.
+  // 요청 된 id, phone 값과 동일한 예약 데이터를 삭제합니다.
   deleteById: (req, res) => {
     // TODO:
 
-    return res.status(200).json(booking);
   }
 };
--- statesairline/controller/flightController.js
+++ statesairline/controller/flightController.js
@@ -2,7 +2,8 @@ const flights = require('../repository/flightList');
 
 module.exports = {
   // [GET] /flight
-  // 요청 된 departure_times, arrival_times, destination, departure 값과 동일한 값을 가진 항공편 데이터를 조회합니다.
+  // 요청 된 파라미터 departure_times, arrival_times 값과 동일한 값을 가진 항공편 데이터를 조회합니다.
+  // 요청 된 파라미터 departure, destination 값과 동일한 값을 가진 항공편 데이터를 조회합니다.
   findAll: (req, res) => {
     // TODO:
 
@@ -15,12 +16,10 @@ module.exports = {
 
   },
 
+  // Advanced
   // [PUT] /flight/:id 요청을 수행합니다.
   // 요청 된 id 값과 동일한 uuid 값을 가진 항공편 데이터를 요쳥 된 Body 데이터로 수정합니다.
   update: (req, res) => {
-    let data;
-    // TODO:
-
-    return res.status(200).json(data);
+     // TODO:
   }
 };
