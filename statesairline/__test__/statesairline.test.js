8b7a7bd01f14379a69341c4efc4912c9607d4410
Jekyung-Min <cwm7485@naver.com>
2022-08-11 17:12:50 +0900

    fix: testcase align with REST API

--- statesairline/__test__/statesairline.test.js
+++ statesairline/__test__/statesairline.test.js
@@ -37,17 +37,14 @@ describe('flight Router', () => {
   });
 
   test('GET /flight 요청의 응답 객체는 `uuid, departure, destination, departure_times, arrival_times`를 포함해야 합니다', function (done) {
+    const props = ['uuid', 'departure', 'destination', 'departure_times', 'arrival_times'];
     return request(app)
       .get('/flight')
       .then(res => {
         const flight = JSON.parse(res.text);
-        expect(flight[0]).toEqual({
-          uuid: 'af6fa55c-da65-47dd-af23-578fdba40bed',
-          departure: 'ICN',
-          destination: 'CJU',
-          departure_times: '2021-12-02T12:00:00',
-          arrival_times: '2021-12-03T12:00:00'
-        });
+        const keys = Object.keys(flight[0])
+        const hasAllKeys = props.every(prop => keys.includes(prop))
+        expect(hasAllKeys).toEqual(true)
         done();
       });
   });
@@ -56,28 +53,11 @@ describe('flight Router', () => {
     return request(app)
       .get('/flight?departure_times=2021-12-03T12:00:00&arrival_times=2021-12-03T12:00:00')
       .then(res => {
-        const flight = JSON.parse(res.text);
-        expect(flight[0]).toEqual({
-          uuid: 'af6fa55c-da65-47dd-af23-578fdba40byd',
-          departure: 'ICN',
-          destination: 'PUS',
-          departure_times: '2021-12-03T12:00:00',
-          arrival_times: '2021-12-03T12:00:00'
-        });
-        expect(flight[1]).toEqual({
-          uuid: 'af6fa55c-da65-47dd-af23-578fdba44bed',
-          departure: 'ICN',
-          destination: 'CJU',
-          departure_times: '2021-12-03T12:00:00',
-          arrival_times: '2021-12-03T12:00:00'
-        });
-        expect(flight[2]).toEqual({
-          uuid: 'af6fa55c-da65-47dd-af23-578fdba41bed',
-          departure: 'CJU',
-          destination: 'ICN',
-          departure_times: '2021-12-03T12:00:00',
-          arrival_times: '2021-12-03T12:00:00'
-        });
+        const flights = JSON.parse(res.text);
+        const departure_times = '2021-12-03T12:00:00';
+        const arrival_times = '2021-12-03T12:00:00';
+        const hasAllRightTimes = flights.every(flight => flight.departure_times === departure_times && flight.arrival_times === arrival_times);
+        expect(hasAllRightTimes).toEqual(true)
         done();
       });
   });
@@ -86,51 +66,25 @@ describe('flight Router', () => {
     return request(app)
       .get('/flight?departure=CJU&destination=ICN')
       .then(res => {
-        const flight = JSON.parse(res.text);
-        expect(flight[0]).toEqual({
-          uuid: 'af6fa55c-da65-47dd-af23-578fdba42bed',
-          departure: 'CJU',
-          destination: 'ICN',
-          departure_times: '2021-12-03T12:00:00',
-          arrival_times: '2021-12-04T12:00:00'
-        });
-        expect(flight[1]).toEqual({
-          uuid: 'af6fa55c-da65-47dd-af23-578fdba41bed',
-          departure: 'CJU',
-          destination: 'ICN',
-          departure_times: '2021-12-03T12:00:00',
-          arrival_times: '2021-12-03T12:00:00'
-        });
-        expect(flight[2]).toEqual({
-          uuid: 'af6fa55c-da65-47dd-af23-578fdba99bed',
-          departure: 'CJU',
-          destination: 'ICN',
-          departure_times: '2021-12-03T12:00:00',
-          arrival_times: '2021-12-04T12:00:00'
-        });
-        expect(flight[3]).toEqual({
-          uuid: 'af6fa55c-da65-47dd-af23-578fdba50bed',
-          departure: 'CJU',
-          destination: 'ICN',
-          departure_times: '2021-12-02T12:00:00',
-          arrival_times: '2021-12-03T12:00:00'
-        });
+        const flights = JSON.parse(res.text);
+        const departure = 'CJU';
+        const destination = 'ICN';
+        const hasAllRightPlace = flights.every(flight => flight.departure === departure && flight.destination === destination);
+        expect(hasAllRightPlace).toEqual(true);
         done();
       });
   });
 
+
   test('GET /flight/:id 요청의 응답 객체는 `uuid, departure, destination, departure_times, arrival_times`를 포함해야 합니다', function (done) {
+    const props = ['uuid', 'departure', 'destination', 'departure_times', 'arrival_times'];
     return request(app)
       .get('/flight/af6fa55c-da65-47dd-af23-578fdba42bed')
       .then(res => {
-        const flight = JSON.parse(res.text);
-        expect(flight[0]).toEqual({
-          uuid: 'af6fa55c-da65-47dd-af23-578fdba42bed',
-          departure: 'CJU',
-          destination: 'ICN',
-          departure_times: '2021-12-03T12:00:00',
-          arrival_times: '2021-12-04T12:00:00'
-        });
+        const flights = JSON.parse(res.text);
+        const keys = Object.keys(flights[0]);
+        const hasAllKeys = props.every(prop => keys.includes(prop));
+        expect(hasAllKeys).toEqual(true)
         done();
       });
   });
@@ -169,73 +123,70 @@ describe('Book Router', () => {
       });
   });
 
-  test('POST /book 요청시, flight_uuid, name, phone 데이터가 객체로 저장되어야 합니다', function (done) {
+
+  test('POST /book 요청시, uuid, name, phone 데이터가 객체로 저장되어야 합니다.', function (done) {
+    const data = {
+      uuid: 'af6fa55c-da65-47dd-af23-578fdba44bed',
+      name: '김코딩',
+      phone: '010-1234-5678'
+    };
     return request(app)
       .post('/book')
-      .send({
-        flight_uuid: 'af6fa55c-da65-47dd-af23-578fdba44bed',
-        name: '김코딩',
-        phone: '010-1234-5678'
-      })
+      .send(data)
       .then(() => {
         return request(app)
           .get('/book')
           .then(res => {
             const bookdata = JSON.parse(res.text);
-            expect(bookdata[0]).toEqual({
-              flight_uuid: 'af6fa55c-da65-47dd-af23-578fdba44bed',
-              name: '김코딩',
-              phone: '010-1234-5678'
-            });
+            const keys = Object.keys(data)
+            const hasAllKeys = keys.every(key => Object.keys(bookdata[0]).includes(key));
+            expect(hasAllKeys).toEqual(true)
+            done();
+          });
+      });
+  });
+  
+  test('GET /book/:phone 요청은 특정 전화번호에 대한 예약 데이터를 반환해야 합니다', function (done) {
+    const data = {
+      uuid: 'af6fa55c-da65-47dd-af23-578fdba50bed',
+      name: '최배열',
+      phone: '010-4321-5678'
+    }
+    return request(app)
+      .post('/book')
+      .send(data)
+      .then(() => {
+        return request(app)
+          .get('/book/010-4321-5678')
+          .then(res => {
+            const bookdata = JSON.parse(res.text);
+            expect(bookdata[0]).toEqual(data);
             done();
           });
       });
   });
 
-  test('GET /book?flight_uuid=af6fa55c-da65-47dd-af23-578fdba50bed 요청은 특정 항공편에 대한 모든 예약 객체를 반환해야 합니다', function (done) {
+  test('GET /book/:id/:phone 요청은 특정 항공편의 전화번호에 대한 예약 데이터를 반환해야 합니다', function (done) {
+    const data = {
+      uuid: 'af6fa55c-da65-47dd-af23-578fdba50bed',
+      name: '최배열',
+      phone: '010-4321-5678'
+    }
     return request(app)
       .post('/book')
-      .send({
-        flight_uuid: 'af6fa55c-da65-47dd-af23-578fdba50bed',
-        name: '최배열',
-        phone: '010-4321-5678'
-      })
+      .send(data)
       .then(() => {
         return request(app)
-          .get('/book?flight_uuid=af6fa55c-da65-47dd-af23-578fdba50bed')
+          .get('/book/af6fa55c-da65-47dd-af23-578fdba50bed/010-4321-5678')
           .then(res => {
             const bookdata = JSON.parse(res.text);
             expect(bookdata[0]).toEqual({
-              flight_uuid: 'af6fa55c-da65-47dd-af23-578fdba50bed',
+              uuid: 'af6fa55c-da65-47dd-af23-578fdba50bed',
               name: '최배열',
               phone: '010-4321-5678'
             });
             done();
           });
-      });
-  });
-
-  test('GET /book?phone=010-1234-5678 요청은 번호에 해당하는 에약 내역을 반환해야 합니다', function (done) {
-    return request(app)
-      .get('/book?phone=010-1234-5678')
-      .then(res => {
-        const bookdata = JSON.parse(res.text);
-        expect(bookdata).toEqual({
-          flight_uuid: 'af6fa55c-da65-47dd-af23-578fdba44bed',
-          name: '김코딩',
-          phone: '010-1234-5678'
-        });
-        done();
-      });
-  });
-
-  test('DELETE /book?phone=010-1234-5678 요청을 하면 예약 목록에서 파라미터 phone에 해당하는 데이터가 삭제되어야 합니다', function (done) {
-    return request(app)
-      .delete('/book?phone=010-1234-5678')
-      .then(res => {
-        const bookdata = JSON.parse(res.text);
-        expect(bookdata.length).toEqual(1);
-        done();
       });
   });
 });
@@ -260,6 +211,26 @@ describe('Advanced Challenges', function () {
           uuid: 'af6fa55c-da65-47dd-af23-578fdba99bed',
           departure: 'ICN',
           destination: 'CJU',
+          departure_times: '2021-12-02T11:00:00',
+          arrival_times: '2021-12-04T15:00:00'
+        });
+        done();
+      });
+  });
+
+  test('PUT /flight/:id 요청의 일부 데이터만 업데이트 된 객체를 반환해야 합니다', function (done) {
+    return request(app)
+      .put('/flight/af6fa55c-da65-47dd-af23-578fdba99bed')
+      .send({
+        departure: 'CJU',
+        destination: 'ICN'
+      })
+      .then(res => {
+        const flight = JSON.parse(res.text);
+        expect(flight).toEqual({
+          uuid: 'af6fa55c-da65-47dd-af23-578fdba99bed',
+          departure: 'CJU',
+          destination: 'ICN',
           departure_times: '2021-12-02T11:00:00',
           arrival_times: '2021-12-04T15:00:00'
         });
