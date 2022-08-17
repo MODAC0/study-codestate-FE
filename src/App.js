90e48e4e4b72bd5af74f70192b59ad8d1bfa865f
Citysquirrel <86960007+Citysquirrel@users.noreply.github.com>
2022-08-17 11:47:33 +0900

    feat: apply global style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
-import Nav from "./components/Nav";
-import ItemListContainer from "./pages/ItemListContainer";
-import NotificationCenter from "./components/NotificationCenter";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ShoppingCart from "./pages/ShoppingCart";
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import ItemListContainer from './pages/ItemListContainer';
+import NotificationCenter from './components/NotificationCenter';
+import './App.css';
+import './variables.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
@@ -15,6 +16,11 @@ function App() {
         <Route path="/shoppingcart" element={<ShoppingCart />} />
       </Routes>
       <NotificationCenter />
+      <img
+        id="logo_foot"
+        src={`${process.env.PUBLIC_URL}/codestates-logo.png`}
+        alt="logo_foot"
+      />
     </Router>
   );
 }
