08dfe643616ff6dcd439917813d615854832a559
Hong Shik Branden Kim <kimploo@gmail.com>
2022-10-11 02:39:18 +0900

    fix: add morgan

--- server/app.js
+++ server/app.js
@@ -3,12 +3,14 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { authToken } = require('./middleware/token');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 dotenv.config();
 
 const app = express();
 const port = 8080;
 
 app.use(express.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 app.use(
   cors({
