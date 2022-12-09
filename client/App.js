d26d336e241751eeb449ac30e48304272d011fc0
MODAC0 <modac0302@gmail.com>
2022-12-09 18:34:10 +0900

    수정

--- client/App.js
+++ client/App.js
@@ -1,36 +1,36 @@
 class App {
   init() {
     document
-      .querySelector('#to-upper-case')
-      .addEventListener('click', this.toUpperCase.bind(this));
+      .querySelector("#to-upper-case")
+      .addEventListener("click", this.toUpperCase.bind(this));
     document
-      .querySelector('#to-lower-case')
-      .addEventListener('click', this.toLowerCase.bind(this));
+      .querySelector("#to-lower-case")
+      .addEventListener("click", this.toLowerCase.bind(this));
   }
   post(path, body) {
-    fetch(`http://localhost:4999/${path}`, {
-      method: 'POST',
+    fetch(`http://localhost:5500/${path}`, {
+      method: "POST",
       body: JSON.stringify(body),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     })
-      .then(res => res.json())
-      .then(res => {
+      .then((res) => res.json())
+      .then((res) => {
         this.render(res);
       });
   }
   toLowerCase() {
-    const text = document.querySelector('.input-text').value;
-    this.post('lower', text);
+    const text = document.querySelector(".input-text").value;
+    this.post("lower", text);
   }
   toUpperCase() {
-    const text = document.querySelector('.input-text').value;
-    this.post('upper', text);
+    const text = document.querySelector(".input-text").value;
+    this.post("upper", text);
   }
   render(response) {
-    const resultWrapper = document.querySelector('#response-wrapper');
-    document.querySelector('.input-text').value = '';
+    const resultWrapper = document.querySelector("#response-wrapper");
+    document.querySelector(".input-text").value = "";
     resultWrapper.innerHTML = response;
   }
 }
--- server/basic-server.js
+++ server/basic-server.js
@@ -1,40 +1,39 @@
 const http = require("http");
-const PORT = 4999;
+
+const PORT = 5500;
+
 const ip = "localhost";
 
-const server = http.createServer((request, response) => {
-  // todo: 클라이언트의 preflight request에 대한 응답을 작성해야 한다.
-  // * 만약 요청 메서드가 options이면
-  if (request.method === "OPTIONS") {
-    // * CORS 설정
-    // writeHead 메서드를 사용하여 200 헤더 데이터를 전송한다
-    response.writeHead(200, defaultCorsHeader);
-    // end 메서드를 사용하여 모든 응답 헤더와 본문이 전송되었음을 서버에 알림.
-    response.end("hello mini-server sprints");
-  }
-  // todo: 요청 메서드가 post, 요청 url이 ../lower일 때 응답.end에는 소문자로 변환된 body값이 담겨야 한다.
-  if (request.method === "POST") {
-    let body = [];
-    request
-      .on("data", (chunk) => {
-        body.push(chunk);
-        // 빈배열에 청크를 담는다.
-      })
-      .on("end", () => {
-        // end 이벤트에서 이어 붙이고 문자열로 변환
-        body = Buffer.concat(body).toString();
-        // writeHead 메서드를 사용하여 201 헤더 데이터를 전송한다
-        response.writeHead(201, defaultCorsHeader);
-        // url의 엔드포인트가 lower일 때 응답데이터에 문자열 body를 소문자로 변환하여 반환
-        if (request.url === "/lower") response.end(body.toLowerCase());
-        // url의 엔드포인트가 upper일 때 응답데이터에 문자열 body를 대문자로 변환하여 반환
-        else if (request.url === "/upper") response.end(body.toUpperCase());
-        //
-        else {
-          response.writeHead(404, defaultCorsHeader);
-          response.end();
-        }
+const server = http.createServer((req, res) => {
+  if (req.method === "POST") {
+    if (req.url === "/lower") {
+      let data = "";
+      req.on("data", (chunk) => {
+        data = data + chunk;
       });
+      req.on("end", () => {
+        data = data.toLowerCase();
+        res.writeHead(201, defaultCorsHeader);
+        res.end(data);
+      });
+    } else if (req.url === "/upper") {
+      let data = "";
+      req.on("data", (chunk) => {
+        data = data + chunk;
+      });
+      req.on("end", () => {
+        data = data.toUpperCase();
+        res.writeHead(201, defaultCorsHeader);
+        res.end(data);
+      });
+    } else {
+      res.writeHead(404, defaultCorsHeader);
+      res.end();
+    }
+  }
+  if (req.method === "OPTIONS") {
+    res.writeHead(200, defaultCorsHeader);
+    res.end();
   }
 });
 
@@ -48,3 +47,59 @@ const defaultCorsHeader = {
   "Access-Control-Allow-Headers": "Content-Type, Accept",
   "Access-Control-Max-Age": 10,
 };
+
+// const PORT = 5500;
+// const ip = "localhost";
+
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+
+// app.use(cors());
+// app.use(express.static("client"));
+// //express.static(root, [options]) - 정적 파일을 제공
+// //루트 인수는 정적 자산을 제공할 루트 디렉토리를 지정, req.url을 제공된 루트 디렉토리와 결합하여 제공할 파일을 결정합니다.
+// app.use(express.json({ strict: false }));
+// //json은 obj; -> 다른 데이터 타입도 허용해주기 위해 false
+
+// //express.json() ->  JSON 페이로드로 들어오는 요청을 구문 분석하고
+
+// //JSON 형태 "{\"sender\":\"김코딩\",\"receiver\":\"박해커\",\"message\":\"해커야 오늘 저녁 같이 먹을래?\"}"
+
+// //바디 파서를 기반으로 합니다.express.json() === JSON.parse()
+
+// //밑에 처럼 다시 사람이 읽을 수 있는 문자로 만드는것
+// //{sender: "김코딩", receiver: "박해커", message: "해커야 오늘 저녁 같이 먹을래?"}
+
+// //반대는 JSON.stringify() 인것, 이건 객체로 받은걸 const message = {sender: "김코딩"}
+// //객체를 JSON으로 변환시킨다. "{"sender":"김코딩"}" 이런식으로!
+
+// //strict이 true일때 배열이나 객체일 때만 파서해준다
+// //strict이 false일때 모든값 허용해서 파서해준다
+// //블로그 참고 https://semtax.tistory.com/7
+
+// //get 메소드일 때
+// app.get("/", function (req, res) {
+//   res.send("hello");
+// });
+
+// //post 메소드일 때
+// app.post("/upper", function (req, res) {
+//   //console.log(req.body); //파싱이 안되면 undefined가 나온다.
+//   let output = req.body; //req.body에는 JSON의 형태로 payload가 담겨져 있습니다.
+//   output = output.toUpperCase(); //대문자로 바꾸고
+//   //console.log(output);
+//   res.json(output);
+// });
+
+// app.post("/lower", function (req, res) {
+//   //console.log(req.body);
+//   let output = req.body; //req.body에는 JSON의 형태로 payload가 담겨져 있습니다.
+//   output = output.toLowerCase();
+//   //console.log(output);
+//   res.json(output);
+// });
+
+// app.listen(PORT, ip, () => {
+//   console.log(`http server listen on ${ip}:${PORT}`);
+// });
