2cb9d116e779ac3abd3b6c3bf56fde100827b8ba
MODAC0 <modac0302@gmail.com>
2022-12-16 13:06:09 +0900

    제출

--- fe-sprint-stringify-json/src/stringifyJSON.js
+++ fe-sprint-stringify-json/src/stringifyJSON.js
@@ -26,9 +26,38 @@
  * 하지만 이 과제의 목적은 재귀를 공부하는 것이니, 처음부터 구현해봐야겠지요?:
  */
 function stringifyJSON(obj) {
-  // your code goes here
-};
+  if (typeof obj === "boolean" || typeof obj === "number" || obj === null) {
+    return String(obj);
+  }
+  if (typeof obj === "string") {
+    return `"${obj}"`;
+  }
+  if (typeof obj === "undefined" || typeof obj === "function") {
+    return "undefined";
+  }
 
+  if (Array.isArray(obj)) {
+    let newArr = []; //push메소드 쓰지 않고 배열의 요소를 stringifyJSON(item)값으로 치환할 때도 구현 가능
+    for (let item of obj) {
+      newArr.push(stringifyJSON(item)); //push만 해주고
+    }
+    return `[${newArr}]`; //`${newArr}` 하면 요소만 나열되고 통째로 문자열
+  }
+
+  if (typeof obj === "object" && obj !== null) {
+    let str = "";
+    for (let key in obj) {
+      if (typeof obj[key] === "function" || obj[key] === undefined) {
+        return "{}";
+      }
+      let newKey = stringifyJSON(key); //key
+      let newValue = stringifyJSON(obj[key]); //key의 값
+      str = str + newKey + ":" + newValue + ",";
+    }
+    str = str.slice(0, -1); //마지막 쉼표 버리기
+    return `{${str}}`;
+  }
+}
 // 다음 코드는 결과 제출을 위한 코드입니다. 신경 쓰지 않아도 좋습니다.
 if (typeof window === "undefined") {
   module.exports = stringifyJSON;
