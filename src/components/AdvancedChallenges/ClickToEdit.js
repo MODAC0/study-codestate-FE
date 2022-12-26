fde3f2d6d09f28e18b9bc53d912dfcca05d01540
MODAC0 <modac0302@gmail.com>
2022-12-26 19:47:05 +0900

    2022.12.26

--- src/components/AdvancedChallenges/ClickToEdit.js
+++ src/components/AdvancedChallenges/ClickToEdit.js
@@ -1,5 +1,5 @@
-import { useEffect, useState, useRef } from 'react';
-import styled from 'styled-components';
+import { useEffect, useState, useRef } from "react";
+import styled from "styled-components";
 
 export const InputBox = styled.div`
   text-align: center;
@@ -42,42 +42,49 @@ export const MyInput = ({ value, handleValueChange }) => {
   useEffect(() => {
     setNewValue(value);
   }, [value]);
-
   const handleClick = () => {
     // TODO : isEditMode 상태를 변경합니다.
+    setEditMode(!isEditMode);
   };
 
   const handleBlur = () => {
     // TODO : Edit가 불가능한 상태로 변경합니다.
+    setEditMode(!isEditMode);
     handleValueChange(newValue);
   };
 
   const handleInputChange = (e) => {
     // TODO : 저장된 value를 업데이트합니다.
+    setNewValue(e.target.value);
   };
 
   return (
     <InputBox>
       {isEditMode ? (
         <InputEdit
-          type='text'
+          type="text"
           value={newValue}
           ref={inputEl}
           // TODO : 포커스를 잃으면 Edit가 불가능한 상태로 변경되는 메소드가 실행되어야 합니다.
           // TODO : 변경 사항이 감지되면 저장된 value를 업데이트 되는 메소드가 실행되어야 합니다.
+          onChange={handleInputChange}
+          onBlur={handleBlur}
         />
       ) : (
-        <span 
-        // TODO : 클릭하면 Edit가 가능한 상태로 변경되어야 합니다.
-        >{newValue}</span>
+        <span
+          // TODO : 클릭하면 Edit가 가능한 상태로 변경되어야 합니다.
+          onClick={handleClick}
+        >
+          {newValue}
+        </span>
       )}
     </InputBox>
   );
-}
+};
 
 const cache = {
-  name: '김코딩',
-  age: 20
+  name: "김코딩",
+  age: 20,
 };
 
 export const ClickToEdit = () => {
@@ -88,14 +95,22 @@ export const ClickToEdit = () => {
     <>
       <InputView>
         <label>이름</label>
-        <MyInput value={name} handleValueChange={(newValue) => setName(newValue)} />
+        <MyInput
+          value={name}
+          handleValueChange={(newValue) => setName(newValue)}
+        />
       </InputView>
       <InputView>
         <label>나이</label>
-        <MyInput value={age} handleValueChange={(newValue) => setAge(newValue)} />
+        <MyInput
+          value={age}
+          handleValueChange={(newValue) => setAge(newValue)}
+        />
       </InputView>
       <InputView>
-        <div className='view'>이름 {name} 나이 {age}</div>
+        <div className="view">
+          이름 {name} 나이 {age}
+        </div>
       </InputView>
     </>
   );
