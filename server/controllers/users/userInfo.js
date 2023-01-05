3fbb68aae5b4f31715683c96e5eff391c7c75d71
MODAC0 <modac0302@gmail.com>
2023-01-05 21:55:49 +0900

    2023.01.05

--- server/controllers/users/userInfo.js
+++ server/controllers/users/userInfo.js
@@ -1,11 +1,14 @@
-const { USER_DATA } = require('../../db/data');
+const { USER_DATA } = require("../../db/data");
 
 module.exports = (req, res) => {
-  /*
-   * TODO: 쿠키 검증 여부에 따라 유저 정보를 전달하는 로직을 구현하세요.
-   *
-   * 로그인 시 설정한 쿠키가 존재하는 지 확인해야 합니다.
-   * 아직 로그인을 하지 않았다면 쿠키가 존재하지 않을 수 있습니다.
-   * 쿠키에 유저의 id가 존재하는지 확인하고 싶다면 콘솔에 req.cookies를 출력해보세요.
-   */
+  const cookieId = req.cookies.cookieId;
+  const userInfo = {
+    ...USER_DATA.filter((user) => user.id === cookieId)[0],
+  };
+  if (!cookieId || !userInfo.id) {
+    res.status(401).send("Not Authorized");
+  } else {
+    delete userInfo.password;
+    res.send(userInfo);
+  }
 };
