a7cc1b8e040c208aaa5bb58da75561a08eee5867
MODAC0 <modac0302@gmail.com>
2023-01-05 17:07:31 +0900

    2023.01.04

--- fe-sprint-auth-cookie/client/src/pages/Login.js
+++ fe-sprint-auth-cookie/client/src/pages/Login.js
@@ -1,66 +1,77 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Login() {
   const [loginInfo, setLoginInfo] = useState({
-    userId: '',
-    password: '',
+    userId: "",
+    password: "",
   });
   const [checkedKeepLogin, setCheckedKeepLogin] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
   const handleInputValue = (key) => (e) => {
     setLoginInfo({ ...loginInfo, [key]: e.target.value });
   };
   const loginRequestHandler = () => {
-    /*
-    TODO: Login 컴포넌트가 가지고 있는 state를 이용해 로그인을 구현합니다.
-    로그인에 필요한 유저정보가 충분히 제공되지 않았다면 에러메시지가 나타나도록 구현하세요.
+    if (!loginInfo.userId || !loginInfo.password) {
+      setErrorMessage("아이디와 비밀번호를 입력하세요");
+      return;
+    }
+    // TODO: Login 컴포넌트가 가지고 있는 state를 이용해 로그인을 구현합니다.
+    // 로그인에 필요한 유저정보가 충분히 제공되지 않았다면 에러메시지가 나타나도록 구현하세요.
     return axios
-      .post(login을 담당하는 endpoint)
+      .post("https://localhost:4000/login", { loginInfo, checkedKeepLogin })
+
       .then((res) => {
-        로그인에 성공했다면 응답으로 받은 데이터가 Mypage에 렌더링되도록 State를 변경하세요.
+        console.log(res.data);
+        // 로그인에 성공했다면 응답으로 받은 데이터가 Mypage에 렌더링되도록 State를 변경하세요.
       })
       .catch((err) => {
-        로그인에 실패했다면 그에 대한 에러 핸들링을 구현하세요. 
+        // 로그인에 실패했다면 그에 대한 에러 핸들링을 구현하세요.
       });
-    */
   };
 
   return (
-    <div className='container'>
-      <div className='left-box'>
+    <div className="container">
+      <div className="left-box">
         <span>
           Education
           <p>for the</p>
           Real World
         </span>
       </div>
-      <div className='right-box'>
+      <div className="right-box">
         <h1>AUTH STATES</h1>
         <form onSubmit={(e) => e.preventDefault()}>
-          <div className='input-field'>
+          <div className="input-field">
             <span>ID</span>
-            <input type='text' data-testid='id-input' onChange={handleInputValue('userId')} />
+            <input
+              type="text"
+              data-testid="id-input"
+              onChange={handleInputValue("userId")}
+            />
             <span>Password</span>
             <input
-              type='password'
-              data-testid='password-input'
-              onChange={handleInputValue('password')}
+              type="password"
+              data-testid="password-input"
+              onChange={handleInputValue("password")}
             />
-            <label className='checkbox-container'>
-              <input type='checkbox' onChange={() => setCheckedKeepLogin(!checkedKeepLogin)} />
-              {' 로그인 상태 유지하기'}
+            <label className="checkbox-container">
+              <input
+                type="checkbox"
+                onChange={() => setCheckedKeepLogin(!checkedKeepLogin)}
+              />
+              {" 로그인 상태 유지하기"}
             </label>
           </div>
-          <button type='submit' onClick={loginRequestHandler}>
+          <button type="submit" onClick={loginRequestHandler}>
             LOGIN
           </button>
           {errorMessage ? (
-            <div id='alert-message' data-testid='alert-message'>
+            <div id="alert-message" data-testid="alert-message">
               {errorMessage}
             </div>
           ) : (
-            ''
+            ""
           )}
         </form>
       </div>
--- fe-sprint-auth-cookie/server/controllers/users/userInfo.js
+++ fe-sprint-auth-cookie/server/controllers/users/userInfo.js
@@ -1,6 +1,17 @@
-const { USER_DATA } = require('../../db/data');
+const { USER_DATA } = require("../../db/data");
 
 module.exports = (req, res) => {
+  const cookieId = req.cookies.cookieId;
+  const userInfo = {
+    ...USER_DATA.filter((user) => user.id === cookieId)[0],
+  };
+  if (!cookieId || !userInfo.id) {
+    res.status(401).send("Not Authorized");
+  } else {
+    // 비밀번호는 민감한 정보라서 삭제 후에 보내야 합니다.
+    delete userInfo.password;
+    res.send(userInfo);
+  }
   /*
    * TODO: 쿠키 검증 여부에 따라 유저 정보를 전달하는 로직을 구현하세요.
    *
