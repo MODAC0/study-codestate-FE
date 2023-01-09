95be01ade72a962a9d10d032e7913e63fead97be
MODAC0 <modac0302@gmail.com>
2023-01-09 10:31:40 +0900

    commit

--- fe-sprint-auth-token/server/controllers/helper/tokenFunctions.js
+++ fe-sprint-auth-token/server/controllers/helper/tokenFunctions.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-const { sign, verify } = require('jsonwebtoken');
+require("dotenv").config();
+const { sign, verify } = require("jsonwebtoken");
 
 module.exports = {
   generateToken: async (user, checkedKeepLogin) => {
@@ -9,13 +9,13 @@ module.exports = {
     };
     let result = {
       accessToken: sign(payload, process.env.ACCESS_SECRET, {
-        expiresIn: '1d', // 1일간 유효한 토큰을 발행합니다.
+        expiresIn: "1d", // 1일간 유효한 토큰을 발행합니다.
       }),
     };
 
     if (checkedKeepLogin) {
       result.refreshToken = sign(payload, process.env.REFRESH_SECRET, {
-        expiresIn: '7d', // 일주일간 유효한 토큰을 발행합니다.
+        expiresIn: "7d", // 일주일간 유효한 토큰을 발행합니다.
       });
     }
     return result;
@@ -23,10 +23,10 @@ module.exports = {
   verifyToken: async (type, token) => {
     let secretKey, decoded;
     switch (type) {
-      case 'access':
+      case "access":
         secretKey = process.env.ACCESS_SECRET;
         break;
-      case 'refresh':
+      case "refresh":
         secretKey = process.env.REFRESH_SECRET;
         break;
       default:
--- fe-sprint-auth-token/server/controllers/users/userInfo.js
+++ fe-sprint-auth-token/server/controllers/users/userInfo.js
@@ -1,6 +1,6 @@
-const { USER_DATA } = require('../../db/data');
+const { USER_DATA } = require("../../db/data");
 // JWT는 verifyToken으로 검증할 수 있습니다. 먼저 tokenFunctions에 작성된 여러 메서드들의 역할을 파악하세요.
-const { verifyToken, generateToken } = require('../helper/tokenFunctions');
+const { verifyToken, generateToken } = require("../helper/tokenFunctions");
 
 module.exports = async (req, res) => {
   /*
@@ -10,4 +10,27 @@ module.exports = async (req, res) => {
    * Access Token이 만료되었다면 Refresh Token을 검증해 Access Token을 재발급하여야 합니다.
    * Access Token과 Refresh Token 모두 만료되었다면 상태 코드 401을 보내야합니다.
    */
+
+  const { access_jwt, refresh_jwt } = req.cookies;
+
+  const accessPayload = await verifyToken("access", access_jwt);
+  const refreshPayload = await verifyToken("refresh", refresh_jwt);
+
+  if (accessPayload) {
+    // 검증이 잘 되었으면 유저정보를 찾는다
+    const userInfo = {
+      ...USER_DATA.filter((user) => user.id === accessPayload.id)[0],
+    };
+    if (!userInfo.id) {
+      res.status(401).send("Not Authorized");
+    }
+    delete userInfo.password;
+    res.send(userInfo);
+  } else if (refresh_jwt) {
+    if (!refreshPayload) {
+      res.status(401).send("Not Authorized");
+    }
+  }
+
+  /* 재발급 토큰을 만드는 함수 verifyToken => 인자로 타입과 토큰 */
 };
