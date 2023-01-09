64c91e4ce8591028796e98fdc4ad9fad06739380
MODAC0 <modac0302@gmail.com>
2023-01-09 15:27:14 +0900

    2023.01.10

--- fe-sprint-auth-oauth/client/src/pages/Login.js
+++ fe-sprint-auth-oauth/client/src/pages/Login.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import githubLogo from './../images/github.png';
+import React from "react";
+import githubLogo from "./../images/github.png";
 
 export default function Login() {
-  const CLIENT_ID = 'FILL_ME_IN';
+  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 
   const loginRequestHandler = () => {
     // TODO: GitHub로부터 사용자 인증을 위해 GitHub로 이동해야 합니다. 적절한 URL을 입력하세요.
@@ -15,21 +15,21 @@ export default function Login() {
 
   return (
     <>
-      <div className='left-box'>
+      <div className="left-box">
         <span>
           Education
           <p>for the</p>
           Real World
         </span>
       </div>
-      <div className='right-box'>
-        <div className='right-box'>
+      <div className="right-box">
+        <div className="right-box">
           <h1>AUTH STATES</h1>
           <h3>OAuth 2.0 소셜 로그인</h3>
           <form onSubmit={(e) => e.preventDefault()}>
-            <div className='input-field'>
-              <button type='submit' onClick={loginRequestHandler}>
-                <img id='logo' alt='logo' src={githubLogo} />
+            <div className="input-field">
+              <button type="submit" onClick={loginRequestHandler}>
+                <img id="logo" alt="logo" src={githubLogo} />
                 <span> LOGIN WITH GITHUB</span>
               </button>
             </div>
