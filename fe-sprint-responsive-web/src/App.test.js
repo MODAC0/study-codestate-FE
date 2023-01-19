104b86cb1aa5f627392c6a404f13a05ad33b32fa
MODAC0 <modac0302@gmail.com>
2023-01-19 10:52:09 +0900

    commit

--- fe-sprint-responsive-web/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-
-test('renders learn react link', () => {
-  const {container} = render(<BrowserRouter><App /></BrowserRouter>);
-  const AppElement = container.querySelector(".App");
-  expect(AppElement).toBeTruthy();
-});
